77eaa9984359f7f8afc7f5b9016594ea83f8536b
oubino <oliver.umney@hotmail.co.uk>
2021-02-20 18:02:10 +0000

    modified evaluate functions in line with other changes

--- settings.py
+++ settings.py
@@ -101,8 +101,8 @@ def init():
     
     # training parameters
 
-    epoch_batch = 10
-    num_epoch_batches = 12
+    epoch_batch = 1
+    num_epoch_batches = 1
     net_features = 8
     
     alpha = 1/25000
