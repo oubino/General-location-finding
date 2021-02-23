75dfdc7933f776cb59a43d522ce9f2e5de77c615
oubino <oliver.umney@hotmail.co.uk>
2021-02-23 14:25:32 +0000

    fixed coding path for VM oli

--- settings.py
+++ settings.py
@@ -60,7 +60,7 @@ def init():
             save_data_path = r'/home/rankinaaron98/data/results/Aaron'
         elif aaron_or_oli == False:
             # Oli paths
-            coding_path = r'/home/olive/General-location-finding'
+            coding_path = r'/home/olive/GitHub/General-location-finding'
             root = r'/home/olive/data/HNSCC_deepmind_cropped'
             save_data_path = r'/home/olive/data/results/Oli'
             
