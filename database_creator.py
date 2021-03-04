b6dec1f3a58f782b15c926a247b665d377b8ce3a
oubino <oliver.umney@hotmail.co.uk>
2021-03-04 12:24:02 +0000

    python file to create a database from the CT scans with delineations

---
+++ database_creator.py
@@ -0,0 +1,14 @@
+import os
+import csv
+
+
+root = r'C:\Users\olive\OneDrive\Documents\Photomed Essay'
+
+files = list(sorted(os.listdir(root)))
+print(files)
+
+with open(r'C:\Users\olive\OneDrive\Documents\CNN\db.csv', 'w', newline='') as csvfile:
+      writer = csv.writer(csvfile)
+      for i in files:
+          writer.writerow([files[files.index(i)] + '.pack'])#)#[index[i]])
+        
