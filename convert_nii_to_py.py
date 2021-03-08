2fa1f013b6614543ea9d5d7d1337df582b3f5c13
rankinaaron98 <rankinaaron98@gmail.com>
2021-03-08 17:45:52 +0000

    changed root so that conversion on vm

--- convert_nii_to_py.py
+++ convert_nii_to_py.py
@@ -2,19 +2,24 @@
 
 import SimpleITK as sitk
 import numpy as np
+import os
 
 # paths
-nii_path = 
-numpy_path =  
+#nii_path = r'C:\Users\ranki_252uikw\Documents\MPhysS2\Facial_asymmetry\CTs'
+#numpy_path =  r'C:\Users\ranki_252uikw\Documents\MPhysS2\Facial_asymmetry\CTs_np'
 
+nii_path = r'/home/rankinaaron98/data/Facial_asymmetry_test_nii'
+numpy_path = r'/home/rankinaaron98/data/Facial_asymmetry_test_np'
 # loop opver nii images
 files = list(sorted(os.listdir(nii_path)))
+print(files)
 for i in files:
     sitk = sitk.ReadImage(i)
+    print('Image %1.0f' % i)
     py_arr = sitk.GetArrayFromImage(sitk)
     np_root = i.replace('.nii','.py')
     np.save(np_root,py_arr)
-    
+    print('--------------------')
 
 # A path to a T1-weighted brain .nii image:
 # t1_fn = './brain_t1_0001.nii'
