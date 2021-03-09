b23839be79826b97a98720dfee43b966a2fa76fd
oubino <59172905+oubino@users.noreply.github.com>
2021-03-09 18:54:03 +0000

    Update convert_nii_to_py.py
    converts to python, flips if needed and creates csv of image dimensions

--- convert_nii_to_py.py
+++ convert_nii_to_py.py
@@ -7,22 +7,27 @@ import os
 # paths
 nii_path = r'C:\Users\ranki_252uikw\Documents\MPhysS2\Facial_asymmetry\landmarks_aaron'
 numpy_path =  r'C:\Users\ranki_252uikw\Documents\MPhysS2\Facial_asymmetry\landmarks_aaron_np'
+csv_path = r'C:\Users\ranki_252uikw\Documents\MPhysS2\Facial_asymmetry\img_dimensions.csv'
 
 #nii_path = r'/home/rankinaaron98/data/Facial_asymmetry/CTs'
 #numpy_path = r'/home/rankinaaron98/data/Facial_asymmetry/CTs_np'
 
-# loop opver nii images
-files = list(sorted(os.listdir(nii_path)))
-print(files)
-for i in files:
-    image = sitk.ReadImage(os.path.join(nii_path,i))
-    print('Image % s' % i)
-    py_arr = sitk.GetArrayFromImage(image)
-    np_root = i.replace('.nii','')
-    numpy_file = os.path.join(numpy_path,np_root)
-    np.save(numpy_file,py_arr)
-    print('--------------------')
-    print(py_arr.shape)
-
-# A path to a T1-weighted brain .nii image:
-# t1_fn = './brain_t1_0001.nii'
+with open(csv_dimensions_path, 'w',newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['Patient','In-plane dimension (mm)','Slice thickness (mm)'])
+    # loop opver nii images
+    files = list(sorted(os.listdir(nii_path)))
+    print(files)
+    for i in files:
+        image = sitk.ReadImage(os.path.join(nii_path,i))
+        print('Image % s' % i)
+        dir = image.GetDirection()
+        if dir[-1] == -1:
+            image = sitk.Flip(image, [False,False,True])
+        writer.writerow([i.replace('.nii', ''),image.GetSpacing()[0],image.GetSpacing()[2]])
+        py_arr = sitk.GetArrayFromImage(image)
+        np_root = i.replace('.nii','')
+        numpy_file = os.path.join(numpy_path,np_root)
+        np.save(numpy_file,py_arr)
+        print(py_arr.shape)
+        print('--------------------')
