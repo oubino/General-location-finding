5f62ada1b6d577ce0e6f26a56e009c45be97d188
oubino <oliver.umney@hotmail.co.uk>
2021-03-10 07:29:07 +0000

    fix

--- functions.py
+++ functions.py
@@ -302,7 +302,8 @@ def point_to_point_mm(mask_x, mask_y, mask_z, pred_x, pred_y, pred_z, patient):
   index = 0
   print('index')
   print(index)
-  for i in len(list_img):
+  
+  for i in range(len(list_img)):
       if list_img[i][0] == pat_ind:
           index = i
   pixel_mm_x = list_img[index][1] # 1 pixel = pixel_mm_x * mm
