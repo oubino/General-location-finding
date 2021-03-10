bfe457bc1e2189f5545c05e1098248347647159f
oubino <oliver.umney@hotmail.co.uk>
2021-03-10 07:54:49 +0000

    remove prints

--- functions.py
+++ functions.py
@@ -309,14 +309,8 @@ def point_to_point_mm(mask_x, mask_y, mask_z, pred_x, pred_y, pred_z, patient):
   pixel_mm_x = list_img[index][1] # 1 pixel = pixel_mm_x * mm
   pixel_mm_y = list_img[index][1]
   pixel_mm_z = list_img[index][2]
-  print('pixel x')
-  print(pixel_mm_x)
   if len(S.downsample_idx_list):
         # array not emtpy
-        print('list')
-        print(S.downsample_idx_list)
-        print('patient')
-        print(patient)
         index = 0
         for i in range(len(S.downsample_idx_list)):
           if S.downsample_idx_list == patient:
