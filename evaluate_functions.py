7eb20568d53fff54fdf9dc995f0107034a9d85e1
oubino <oliver.umney@hotmail.co.uk>
2021-03-10 06:21:09 +0000

    mod

--- evaluate_functions.py
+++ evaluate_functions.py
@@ -307,7 +307,7 @@ def performance_metrics(model,sigmas,gamma, epochs_completed):
             
 
           #img_landmark_point_to_point = point_to_point(structure_max_x, structure_max_y, structure_max_z, pred_max_x, pred_max_y, pred_max_z)
-          img_landmark_point_to_point = functions.point_to_point_mm(structure_max_x, structure_max_y, structure_max_z, pred_max_x, pred_max_y, pred_max_z, idx[i].item())
+          img_landmark_point_to_point = functions.point_to_point_mm(structure_max_x, structure_max_y, structure_max_z, pred_max_x, pred_max_y, pred_max_z, idx[i])
           p2p_landmarks[l] = np.append(p2p_landmarks[l],img_landmark_point_to_point.cpu())
           # if img_point_to_point > 20mm is an outlier
           if img_landmark_point_to_point > 20:
