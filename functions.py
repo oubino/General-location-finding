4b1bcadff2cac2037c81e55c0a9914df65ed1c13
oubino <oliver.umney@hotmail.co.uk>
2021-03-10 07:35:34 +0000

    fix

--- functions.py
+++ functions.py
@@ -309,9 +309,13 @@ def point_to_point_mm(mask_x, mask_y, mask_z, pred_x, pred_y, pred_z, patient):
   pixel_mm_x = list_img[index][1] # 1 pixel = pixel_mm_x * mm
   pixel_mm_y = list_img[index][1]
   pixel_mm_z = list_img[index][2]
+  print('pixel x')
+  print(pixel_mm_x)
   if len(S.downsample_idx_list):
         # array not emtpy
         index = np.where(S.downsample_idx_list==patient)
+        print('index')
+        print(index)
         #print('pre amendment pixel sizes', pixel_mm_x, pixel_mm_y, pixel_mm_z)
         pixel_mm_x = float(pixel_mm_x) * S.downsample_ratio_w[index[0][0]]
         pixel_mm_y = float(pixel_mm_y) * S.downsample_ratio_h[index[0][0]]
