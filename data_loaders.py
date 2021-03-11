278a54e32bea372dfa3c15cb79fc1c86ca8b1ec8
oubino <oliver.umney@hotmail.co.uk>
2021-03-11 19:43:06 +0000

    fix

--- data_loaders.py
+++ data_loaders.py
@@ -98,7 +98,7 @@ if S.print_CT_check == True:
     for i in range(len(val_set)):
         for landmark in S.landmarks:
             structure = val_set.__getitem__(i)['structure'].squeeze(0)
-            structure_extrac = eval_func.extract_landmark_for_structure(structure, landmark).cpu().numpy()
+            structure_extrac = eval_func.extract_landmark_for_structure_np(structure, landmark).cpu().numpy()
             eval_func.plot_3d_pred_img_no_pred(val_set.__getitem__(i)['image'].squeeze(0), structure_extrac, S.threshold_img_print, val_path_ct, val_set.__getitem__(i)['patient'], landmark)
         print(val_set.__getitem__(i)['patient'])
     
