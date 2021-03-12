7ee8522d9165e9e4e51e95700383e1e239ffb495
oubino <oliver.umney@hotmail.co.uk>
2021-03-12 20:15:20 +0000

    fix up

--- load_model.py
+++ load_model.py
@@ -52,7 +52,6 @@ def freeze_layers():
             param.requires_grad = False
     model_froze = nn.Sequential(
         *list(model_load_in.children())[:-1],
-        model_load_in,
         network.OutConv(10,10).to(S.device)
         )
     summary(model_froze, input_size=(1, S.in_y, S.in_x, S.in_z))
