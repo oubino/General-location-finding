54cdfa158859974f97a4c9f4abd089cd8320d347
oubino <oliver.umney@hotmail.co.uk>
2021-03-13 13:36:24 +0000

    train my model for 50 epochs with image 145 removed

--- settings.py
+++ settings.py
@@ -149,7 +149,7 @@ def init():
     downsample_user = True
     
     # run folder
-    run_folder = "run_12_mar_aaron_model_his_data"
+    run_folder = "run_13_mar_my_model_50_epochs"
     run_path = os.path.join(save_data_path, run_folder) 
     try:  
         os.mkdir(run_path)  
@@ -157,7 +157,7 @@ def init():
         print(error) 
     
     # load model path
-    run_folder_load = "run_12_mar_aaron_model_his_data"
+    run_folder_load = "run_13_mar_my_model_50_epochs"
     epoch_load = str(70) 
 
     
