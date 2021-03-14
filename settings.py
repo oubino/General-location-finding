235d6cc1ebbf12b0be15ebe2c76218f339b3f889
oubino <oliver.umney@hotmail.co.uk>
2021-03-14 22:01:12 +0000

    train from 50 to 60 epochs frozen with all datapoints using

--- settings.py
+++ settings.py
@@ -103,8 +103,8 @@ def init():
     batch_size = 1
     
     # specify landmarks + region want to train for
-    landmarks = [1,3,5,7,9] # brainstem # not general
-    landmarks_loc = {1:'com', 3: 'com', 5:'com', 7: 'com', 9:'com', } 
+    landmarks = [1,2,3,4,5,6,7,8,9,10] # brainstem # not general
+    landmarks_loc = {1:'com',2:'com', 3: 'com',4:'com', 5:'com',6:'com', 7: 'com',8:'com', 9:'com',10:'com', } 
     
     # specify all structures which are actually in image
     
@@ -134,7 +134,7 @@ def init():
     
     # training parameters
     epoch_batch = 10
-    num_epoch_batches = 5
+    num_epoch_batches = 1
     net_features = 32
     scnet_feat = 64
     
