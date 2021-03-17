6475d19dda9e9b4a4d2ae63f27bb527a57e41fe4
oubino <oliver.umney@hotmail.co.uk>
2021-03-17 19:38:26 +0000

    load in save common cts/structures to both aaron and Oli

---
+++ common_image.py
@@ -0,0 +1,45 @@
+# save common images for Aaron and Oli
+
+import settings as S
+import numpy as np
+import os
+import math
+import csv
+
+
+# paths
+aaron_structures_load = r'/home/rankinaaron98/data/Facial_asymmetry_aaron/Structures'
+aaron_cts_load = r'/home/rankinaaron98/data/Facial_asymmetry_aaron/CTs'
+oli_structures_load = r'/home/olive/data/Facial_asymmetry_oli/Structures'
+oli_cts_load = r'/home/olive/data/Facial_asymmetry_oli/CTs'
+
+aaron_structures_save = r'/home/rankinaaron98/data/Facial_asymmetry_aaron_common/Structures'
+aaron_cts_save = r'/home/rankinaaron98/data/Facial_asymmetry_aaron_common/CTs'
+oli_structures_save = r'/home/olive/data/Facial_asymmetry_oli_common/Structures'
+oli_cts_save = r'/home/olive/data/Facial_asymmetry_oli_common/CTs'
+
+
+# loop over structures
+files_aaron = list(sorted(os.listdir(aaron_structures_load)))
+files_oli = list(sorted(os.listdir(oli_structures_load)))
+
+# common files
+list_1 = [x for x in files_aaron if x in files_oli]
+
+# for common structures save CT into folder and structure
+for i in list_1:
+    # load cts
+    aaron_ct = np.load(os.path.join(aaron_structures_load,i))
+    oli_ct = np.load(os.path.join(oli_structures_load,i))
+    # load structures
+    aaron_structure = np.load(os.path.join(aaron_cts_load,i))
+    oli_structure = np.load(os.path.join(oli_cts_load,i))
+    # save cts
+    np.save(os.path.join(oli_cts_save,i), oli_ct)
+    np.save(os.path.join(aaron_cts_save,i), aaron_ct)  
+    # save structures
+    np.save(os.path.join(oli_structures_save,i), oli_structure)
+    np.save(os.path.join(aaron_structures_save,i), aaron_structure)  
+    
+        
+# still need to add in CSV for both
