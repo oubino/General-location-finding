9f3686c77bbd8c991363193c5bba7eaa9358b747
rankinaaron98 <rankinaaron98@gmail.com>
2021-03-19 23:28:51 +0000

    train 100 epochs aaron

--- settings.py
+++ settings.py
@@ -68,7 +68,7 @@ def init():
             elif combined_data == False:
                 # Aaron paths
                 coding_path = r'/home/rankinaaron98/General-location-finding'
-                root = r'/home/rankinaaron98/data/Facial_asymmetry_aaron_commons'
+                root = r'/home/rankinaaron98/data/Facial_asymmetry_aaron_common'
                 save_data_path = r'/home/rankinaaron98/data/results/Aaron'
         # load model path
     
