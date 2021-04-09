740ade18c8a98a01c2339da1be071900aa25088c
rankinaaron98 <rankinaaron98@gmail.com>
2021-04-09 15:00:55 +0100

    print 2d cts check

--- data_loading/data_loaders.py
+++ data_loading/data_loaders.py
@@ -230,11 +230,13 @@ if S.print_CT_check == True:
         idx = batch['idx']  
         print(idx)
         patient = batch['patient']
+        print(patient)
 
         for landmark in S.landmarks:
             #patient  = dataset.__getitem__(i)['patient'].squeeze(0)
             structure_loc = functions.landmark_loc(labels, landmark)[0]
             structure_max_x, structure_max_y, structure_max_z = structure_loc[0][0],structure_loc[0][1], structure_loc[0][2] 
+            print(structure_max_x, structure_max_y, structure_max_z)
             #structure_extrac = eval_func.extract_landmark_for_structure_np(structure, landmark)
             #eval_func.plot_3d_pred_img_no_pred(inputs[0].squeeze(0).cpu().numpy(), empty_struc, S.threshold_img_print, path_print_ct, patient[0], landmark)
             #print(dataset.__getitem__(i)['patient'])
