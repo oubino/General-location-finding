4faa7a8481f5875834ddac711b08dd61527ba413
rankinaaron98 <rankinaaron98@gmail.com>
2021-04-09 18:00:58 +0100

    change compare path

--- compare/compare_clicks.py
+++ compare/compare_clicks.py
@@ -87,7 +87,7 @@ save_ct_folder = r'/home/olive/data/Facial_asymmetry_combined_line/CTs'
 load_ct_folder = r'/home/olive/data/Facial_asymmetry_oli/CTs'
 csv_root = r'/home/rankinaaron98/data/Facial_asymmetry_aaron_reclicks/'
 
-hist_root = r'/home/olive/data/Compare/Histograms/'
+hist_root = r'/home/olive/data/Compare/Histograms_aaron/'
 
 # make folders
 try:  
