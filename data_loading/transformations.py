aa3f59cd56c949b65ee9d834d8d0825dc19c8b77
oubino <oliver.umney@hotmail.co.uk>
2021-04-14 22:21:23 +0100

    failsafe if crop out

--- data_loading/transformations.py
+++ data_loading/transformations.py
@@ -96,8 +96,12 @@ class CentreCrop(object):
           y = y - y_left
           z = z - z_left
           coords[l]['x'] = x
-          coords[l]['y']= y
+          coords[l]['y']=  y
           coords[l]['z'] = z
+    
+          # failsafe
+          if x < 0 or x >= S.in_x or y < 0 or y >= S.in_y or z < 0 or z >= S.in_z:
+              exit()
                         
       image_crop = skimage.util.crop(image, ((z_left,z_right),(y_left, y_right), (x_left, x_right)))
           
