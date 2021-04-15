ad20e538058e6f708bd4fc4cf2966d00b2292374
oubino <oliver.umney@hotmail.co.uk>
2021-04-15 01:53:34 +0100

    trial not extra enc/dec

--- model/network.py
+++ model/network.py
@@ -69,7 +69,7 @@ class OutConv(nn.Module):
     def forward(self, x):
         return self.conv(x)
 
-
+"""
 class UNet3d(nn.Module):
     def __init__(self, in_channels, n_classes, s_channels):
         super().__init__()
@@ -107,6 +107,40 @@ class UNet3d(nn.Module):
         output = self.out(x9)
         return output 
     
+"""
+    
+class UNet3d(nn.Module):
+    def __init__(self, in_channels, n_classes, s_channels):
+        super().__init__()
+        self.in_channels = in_channels
+        self.n_classes = n_classes
+        self.s_channels = s_channels
+
+        self.conv = ConvUnit(in_channels, s_channels)
+        self.enc1 = EncoderUnit(s_channels, 2 * s_channels)
+        self.enc2 = EncoderUnit(2 * s_channels, 4 * s_channels)
+        self.enc3 = EncoderUnit(4 * s_channels, 8 * s_channels)
+        self.enc4 = EncoderUnit(8 * s_channels, 8 * s_channels)
+
+        self.dec1 = DecoderUnit(16 * s_channels, 4 * s_channels)
+        self.dec2 = DecoderUnit(8 * s_channels, 2 * s_channels)
+        self.dec3 = DecoderUnit(4 * s_channels, s_channels)
+        self.dec4 = DecoderUnit(2 * s_channels, s_channels)
+        self.out = OutConv(s_channels, n_classes)
+
+    def forward(self, x):
+        x1 = self.conv(x)
+        x2 = self.enc1(x1)
+        x3 = self.enc2(x2)
+        x4 = self.enc3(x3)
+        x5 = self.enc4(x4)
+
+        x6 = self.dec1(x5, x4)
+        x7 = self.dec2(x6, x3)
+        x8 = self.dec3(x7, x2)
+        x9 = self.dec4(x8, x1)
+        output = self.out(x9)
+        return output 
     
 class Transfer_model(nn.Module):
     def __init__(self, n_classes, s_channels, pre_trained_model):
