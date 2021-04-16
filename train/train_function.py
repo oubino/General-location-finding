11d10b94955d6b7df0ad73543988b19f57af7381
rankinaaron98 <rankinaaron98@gmail.com>
2021-04-16 12:44:53 +0100

    sampler

--- train/train_function.py
+++ train/train_function.py
@@ -80,7 +80,7 @@ def train_model(model,scaler, optimizer, scheduler,alpha,reg,gamma,sigmas,num_ep
                                 print((data_loaders.batch_acc_batches - S.batch_acc_steps*iters_to_acc))
                                 loss = loss/((data_loaders.batch_acc_batches - S.batch_acc_steps*iters_to_acc))
                             else:
-                                print(iters_to_acc)
+                                print(iters_to_acc, i)
                                 loss = loss/iters_to_acc
 
 
@@ -89,6 +89,7 @@ def train_model(model,scaler, optimizer, scheduler,alpha,reg,gamma,sigmas,num_ep
                             start_time = time.time()
                             scaler.scale(loss).backward()
                             print(time.time()-start_time)
+                            print(i+1 % data_loaders.batch_acc_batches, i+1 % iters_to_acc )
                             if (i+1 % data_loaders.batch_acc_batches == 0) or (i+1 % iters_to_acc == 0):
                                 print('reached', data_loaders.batch_acc_batches, i+1)
                                 start_time_op = time.time()
