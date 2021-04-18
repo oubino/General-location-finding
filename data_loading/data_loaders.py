8768fe83af3cbe1538f22db1848d9027f01feef3
oubino <oliver.umney@hotmail.co.uk>
2021-04-18 11:27:26 +0100

    double check what this commit is

--- data_loading/data_loaders.py
+++ data_loading/data_loaders.py
@@ -53,57 +53,8 @@ def init(fold, train_ids, test_ids):
     'test': DataLoader(dataset, batch_size=S.batch_size_test, sampler= test_subsampler),
     'val': DataLoader(dataset, batch_size=S.batch_size, sampler= val_subsampler)  
     }
-    
-def init_no_k_fold():
-    # split data in train/val/test
-    train_size = int(0.8 * len(dataset))
-    val_size = int(0.1 * len(dataset))
-    test_size = len(dataset) - train_size - val_size
-    train_set, val_set, test_set = torch.utils.data.random_split(dataset, [train_size, val_size, test_size], generator=torch.Generator().manual_seed(0))
-    # manual seed ensures that same split everytime to ensure testing on correct dataset!
-    # i.e. if random split, train, save, random split, test -> may end up testing on same as training!
-    
-    global batch_acc_batches   
-    batch_acc_batches = math.ceil(train_size/S.batch_size) # rounds it up
-    
-    global image_datasets
-    image_datasets = {
-        'train': train_set, 'val': val_set, 'test':test_set
-    }
-    
-    global dataloaders
-    # Load data in
-    dataloaders = {
-        'train': DataLoader(train_set, batch_size=S.batch_size, num_workers=0),
-        'val': DataLoader(val_set, batch_size=S.batch_size,  num_workers=0),
-        'test': DataLoader(test_set,batch_size = S.batch_size_test, num_workers=0)
-    }
 
-def init_load_no_k_fold():
-    # split data in train/val/test
-    train_size = int(0.8 * len(dataset))
-    val_size = int(0.1 * len(dataset))
-    test_size = len(dataset) - train_size - val_size
-    train_set, val_set, test_set = torch.utils.data.random_split(dataset, [train_size, val_size, test_size], generator=torch.Generator().manual_seed(0))
-    # manual seed ensures that same split everytime to ensure testing on correct dataset!
-    # i.e. if random split, train, save, random split, test -> may end up testing on same as training!
-    
-    global batch_acc_batches  
-    batch_acc_batches = math.ceil(train_size/S.batch_size) # rounds it up
-    
-    global image_datasets
-    image_datasets = {
-        'train': train_set, 'val': val_set, 'test':test_set
-    }
-    
-    global dataloaders
-    # Load data in
-    dataloaders = {
-        'train': DataLoader(train_set, batch_size=S.batch_size, num_workers=0),
-        'val': DataLoader(val_set, batch_size=S.batch_size, num_workers=0),
-        'test': DataLoader(test_set,batch_size = S.batch_size_test, num_workers=0)
-    }
-    
+
 def init_load_k_fold(fold):
     # initialise dataloader 
     kfold = KFold(n_splits = S.folds_trained_with, shuffle = False)
@@ -151,5 +102,57 @@ def init_reserved_test_set():
         'test': DataLoader(test_set,batch_size = S.batch_size_test, shuffle = False, num_workers=0)
     }
     
+"""
+def init_no_k_fold():
+    # split data in train/val/test
+    train_size = int(0.8 * len(dataset))
+    val_size = int(0.1 * len(dataset))
+    test_size = len(dataset) - train_size - val_size
+    train_set, val_set, test_set = torch.utils.data.random_split(dataset, [train_size, val_size, test_size], generator=torch.Generator().manual_seed(0))
+    # manual seed ensures that same split everytime to ensure testing on correct dataset!
+    # i.e. if random split, train, save, random split, test -> may end up testing on same as training!
+    
+    global batch_acc_batches   
+    batch_acc_batches = math.ceil(train_size/S.batch_size) # rounds it up
+    
+    global image_datasets
+    image_datasets = {
+        'train': train_set, 'val': val_set, 'test':test_set
+    }
+    
+    global dataloaders
+    # Load data in
+    dataloaders = {
+        'train': DataLoader(train_set, batch_size=S.batch_size, shuffle = True, num_workers=0),
+        'val': DataLoader(val_set, batch_size=S.batch_size, shuffle =True, num_workers=0),
+        'test': DataLoader(test_set,batch_size = S.batch_size_test, shuffle = False, num_workers=0)
+    }
+
+def init_load_no_k_fold():
+    # split data in train/val/test
+    train_size = int(0.8 * len(dataset))
+    val_size = int(0.1 * len(dataset))
+    test_size = len(dataset) - train_size - val_size
+    train_set, val_set, test_set = torch.utils.data.random_split(dataset, [train_size, val_size, test_size], generator=torch.Generator().manual_seed(0))
+    # manual seed ensures that same split everytime to ensure testing on correct dataset!
+    # i.e. if random split, train, save, random split, test -> may end up testing on same as training!
+    
+    global batch_acc_batches  
+    batch_acc_batches = math.ceil(train_size/S.batch_size) # rounds it up
+    
+    global image_datasets
+    image_datasets = {
+        'train': train_set, 'val': val_set, 'test':test_set
+    }
+    
+    global dataloaders
+    # Load data in
+    dataloaders = {
+        'train': DataLoader(train_set, batch_size=S.batch_size, shuffle = True, num_workers=0),
+        'val': DataLoader(val_set, batch_size=S.batch_size, shuffle = True, num_workers=0),
+        'test': DataLoader(test_set,batch_size = S.batch_size_test, shuffle = False, num_workers=0)
+    }
+    
+"""   
 print('Coding directory: ')
 print(os.getcwd())
