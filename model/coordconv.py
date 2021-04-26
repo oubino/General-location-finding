30bd6a9aaef43f51f9a5521778020c40175af019
oubino <oliver.umney@hotmail.co.uk>
2021-04-26 22:21:11 +0100

    fix up coord conv

--- model/coordconv.py
+++ model/coordconv.py
@@ -4,156 +4,83 @@ import torch.nn.modules.conv as conv
 
 
 class AddCoords(nn.Module):
-    def __init__(self, rank, with_r=False, use_cuda=True):
+    def __init__(self, with_r=False, use_cuda=True):
         super(AddCoords, self).__init__()
-        self.rank = rank
         self.with_r = with_r
         self.use_cuda = use_cuda
 
     def forward(self, input_tensor):
         """
-        :param input_tensor: shape (N, C_in, H, W)
+        :param input_tensor: shape (N, C_in, H, W, D)
         :return:
         """
-        if self.rank == 1:
-            batch_size_shape, channel_in_shape, dim_x = input_tensor.shape
-            xx_range = torch.arange(dim_x, dtype=torch.int32)
-            xx_channel = xx_range[None, None, :]
+        batch_size_tensor, channel_in_shape, y_dim, x_dim, z_dim = input_tensor.shape
+        
+        xx_ones = torch.ones([batch_size_tensor, x_dim], dtype=torch.float32) # e.g. (batch, x_dim)
+        xx_ones = torch.unsqueeze(xx_ones, -1)      # e.g. (batch, x_dim, 1)
+        a = torch.unsqueeze(torch.arange(y_dim, dtype = torch.float32), 0)
+        xy_range = a.repeat([batch_size_tensor, 1]) # e.g. (batch, y_dim)
+        xy_range = torch.unsqueeze(xy_range, 1)  # e.g. (batch, 1, y_dim)
+        
+        xx_channel = torch.matmul(xx_ones, xy_range) # e.g. (batch, x_dim, y_dim, 1)
+        xx_channel = torch.unsqueeze(xx_channel, -1)
+        
+        xx_channel = xx_channel.repeat(1,1,1,z_dim)
+        xx_channel = torch.unsqueeze(xx_channel, -1)             # e.g. (batch, x_dim, y_dim, z_dim, 1)
+        
+        yy_ones = torch.ones([batch_size_tensor, y_dim], dtype=torch.int32)  # e.g. (batch, y_dim)
+        yy_ones = torch.unsqueeze(yy_ones, -1)                    # e.g. (batch, y_dim, 1)
+        b = torch.unsqueeze(torch.arange(z_dim, dtype = torch.int32), 0)
+        yz_range = b.repeat([batch_size_tensor, 1])             # (batch, z_dim)
+        yz_range = torch.unsqueeze(yz_range, 1)                 # e.g. (batch, 1, z_dim)
+        
+        
+        yy_channel = torch.matmul(yy_ones, yz_range)               # e.g. (batch, y_dim, z_dim)
+        yy_channel = torch.unsqueeze(yy_channel, 1)             # e.g. (batch, 1, y_dim, z_dim)
+        
+        yy_channel = yy_channel.repeat(1,x_dim,1,1)
+        yy_channel = torch.unsqueeze(yy_channel, -1)  # e.g. (batch, x_dim, y_dim, z_dim , 1)
+        
+        zz_ones = torch.ones([batch_size_tensor, z_dim], dtype=torch.int32)  # e.g. (batch, z_dim)
+        zz_ones = torch.unsqueeze(zz_ones, 1)                    # e.g. (batch, 1, z_dim)
+        c = torch.unsqueeze(torch.arange(x_dim, dtype = torch.int32), 0)
+        xz_range = c.repeat([batch_size_tensor, 1])             # (batch, x_dim)
+        xz_range = torch.unsqueeze(xz_range, -1)                 # e.g. (batch, x_dim, 1)
+        
+        zz_channel = torch.matmul(xz_range, zz_ones)               # e.g. (batch, x_dim, z_dim)
+        zz_channel = torch.unsqueeze(zz_channel, 2)             # e.g. (batch, x_dim, 1, z_dim)
+        
+        zz_channel = zz_channel.repeat(1,1,y_dim,1)
+        zz_channel = torch.unsqueeze(zz_channel, -1)  # e.g. (batch, x_dim, y_dim, z_dim , 1)
+        
+        
+        xx_channel = (xx_channel) / (y_dim - 1)
+        yy_channel = (yy_channel) / (z_dim - 1)
+        zz_channel = (zz_channel) / (x_dim - 1)
+        
+        xx_channel = xx_channel*2 - 1                           # [-1,1]
+        yy_channel = yy_channel*2 - 1
+        zz_channel = zz_channel*2 - 1
+        
+        xx_channel = xx_channel.permute(0,4,2,1,3)
+        yy_channel = yy_channel.permute(0,4,2,1,3)
+        zz_channel = zz_channel.permute(0,4,2,1,3)
+        
+        
+        xx_channel_mod = zz_channel.cuda()
+        zz_channel_mod = yy_channel.cuda()
+        yy_channel_mod = xx_channel.cuda()
+        
+        out = torch.cat([input_tensor, yy_channel_mod, xx_channel_mod, zz_channel_mod], axis=1)    # e.g. (batch, c+3, y_dim, x_dim, z_dim)
 
-            xx_channel = xx_channel.float() / (dim_x - 1)
-            xx_channel = xx_channel * 2 - 1
-            xx_channel = xx_channel.repeat(batch_size_shape, 1, 1)
-
-            if torch.cuda.is_available and self.use_cuda:
-                input_tensor = input_tensor.cuda()
-                xx_channel = xx_channel.cuda()
-            out = torch.cat([input_tensor, xx_channel], dim=1)
-
-            if self.with_r:
-                rr = torch.sqrt(torch.pow(xx_channel - 0.5, 2))
-                out = torch.cat([out, rr], dim=1)
-
-        elif self.rank == 2:
-            batch_size_shape, channel_in_shape, dim_y, dim_x = input_tensor.shape
-            xx_ones = torch.ones([1, 1, 1, dim_x], dtype=torch.int32)
-            yy_ones = torch.ones([1, 1, 1, dim_y], dtype=torch.int32)
-
-            xx_range = torch.arange(dim_y, dtype=torch.int32)
-            yy_range = torch.arange(dim_x, dtype=torch.int32)
-            xx_range = xx_range[None, None, :, None]
-            yy_range = yy_range[None, None, :, None]
-
-            xx_channel = torch.matmul(xx_range, xx_ones)
-            yy_channel = torch.matmul(yy_range, yy_ones)
-
-            # transpose y
-            yy_channel = yy_channel.permute(0, 1, 3, 2)
-
-            xx_channel = xx_channel.float() / (dim_y - 1)
-            yy_channel = yy_channel.float() / (dim_x - 1)
-
-            xx_channel = xx_channel * 2 - 1
-            yy_channel = yy_channel * 2 - 1
-
-            xx_channel = xx_channel.repeat(batch_size_shape, 1, 1, 1)
-            yy_channel = yy_channel.repeat(batch_size_shape, 1, 1, 1)
-
-            if torch.cuda.is_available and self.use_cuda:
-                input_tensor = input_tensor.cuda()
-                xx_channel = xx_channel.cuda()
-                yy_channel = yy_channel.cuda()
-            out = torch.cat([input_tensor, xx_channel, yy_channel], dim=1)
-
-            if self.with_r:
-                rr = torch.sqrt(torch.pow(xx_channel - 0.5, 2) + torch.pow(yy_channel - 0.5, 2))
-                out = torch.cat([out, rr], dim=1)
-
-        elif self.rank == 3:
-            batch_size_shape, channel_in_shape, dim_z, dim_y, dim_x = input_tensor.shape
-            xx_ones = torch.ones([1, 1, 1, 1, dim_x], dtype=torch.int32)
-            yy_ones = torch.ones([1, 1, 1, 1, dim_y], dtype=torch.int32)
-            zz_ones = torch.ones([1, 1, 1, 1, dim_z], dtype=torch.int32)
-
-            xy_range = torch.arange(dim_y, dtype=torch.int32)
-            xy_range = xy_range[None, None, None, :, None]
-
-            yz_range = torch.arange(dim_z, dtype=torch.int32)
-            yz_range = yz_range[None, None, None, :, None]
-
-            zx_range = torch.arange(dim_x, dtype=torch.int32)
-            zx_range = zx_range[None, None, None, :, None]
-
-            xy_channel = torch.matmul(xy_range, xx_ones)
-            xx_channel = torch.cat([xy_channel + i for i in range(dim_z)], dim=2)
-
-            yz_channel = torch.matmul(yz_range, yy_ones)
-            yz_channel = yz_channel.permute(0, 1, 3, 4, 2)
-            yy_channel = torch.cat([yz_channel + i for i in range(dim_x)], dim=4)
-
-            zx_channel = torch.matmul(zx_range, zz_ones)
-            zx_channel = zx_channel.permute(0, 1, 4, 2, 3)
-            zz_channel = torch.cat([zx_channel + i for i in range(dim_y)], dim=3)
-
-            if torch.cuda.is_available and self.use_cuda:
-                input_tensor = input_tensor.cuda()
-                xx_channel = xx_channel.cuda()
-                yy_channel = yy_channel.cuda()
-                zz_channel = zz_channel.cuda()
-            out = torch.cat([input_tensor, xx_channel, yy_channel, zz_channel], dim=1)
-
-            if self.with_r:
-                rr = torch.sqrt(torch.pow(xx_channel - 0.5, 2) +
-                                torch.pow(yy_channel - 0.5, 2) +
-                                torch.pow(zz_channel - 0.5, 2))
-                out = torch.cat([out, rr], dim=1)
-        else:
-            raise NotImplementedError
+        if self.with_r:
+            rr = torch.sqrt(torch.pow(xx_channel_mod - 0.5, 2) +
+                            torch.pow(yy_channel_mod - 0.5, 2) +
+                            torch.pow(zz_channel_mod - 0.5, 2))
+            out = torch.cat([out, rr], dim=1)
 
         return out
 
-
-class CoordConv1d(conv.Conv1d):
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
-                 padding=0, dilation=1, groups=1, bias=True, with_r=False, use_cuda=True):
-        super(CoordConv1d, self).__init__(in_channels, out_channels, kernel_size,
-                                          stride, padding, dilation, groups, bias)
-        self.rank = 1
-        self.addcoords = AddCoords(self.rank, with_r, use_cuda=use_cuda)
-        self.conv = nn.Conv1d(in_channels + self.rank + int(with_r), out_channels,
-                              kernel_size, stride, padding, dilation, groups, bias)
-
-    def forward(self, input_tensor):
-        """
-        input_tensor_shape: (N, C_in,H,W)
-        output_tensor_shape: N,C_out,H_out,W_out）
-        :return: CoordConv2d Result
-        """
-        out = self.addcoords(input_tensor)
-        out = self.conv(out)
-
-        return out
-
-
-class CoordConv2d(conv.Conv2d):
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
-                 padding=0, dilation=1, groups=1, bias=True, with_r=False, use_cuda=True):
-        super(CoordConv2d, self).__init__(in_channels, out_channels, kernel_size,
-                                          stride, padding, dilation, groups, bias)
-        self.rank = 2
-        self.addcoords = AddCoords(self.rank, with_r, use_cuda=use_cuda)
-        self.conv = nn.Conv2d(in_channels + self.rank + int(with_r), out_channels,
-                              kernel_size, stride, padding, dilation, groups, bias)
-
-    def forward(self, input_tensor):
-        """
-        input_tensor_shape: (N, C_in,H,W)
-        output_tensor_shape: N,C_out,H_out,W_out）
-        :return: CoordConv2d Result
-        """
-        out = self.addcoords(input_tensor)
-        out = self.conv(out)
-
-        return out
 
 
 class CoordConv3d(conv.Conv3d):
@@ -161,16 +88,15 @@ class CoordConv3d(conv.Conv3d):
                  padding=0, dilation=1, groups=1, bias=True, with_r=False, use_cuda=True):
         super(CoordConv3d, self).__init__(in_channels, out_channels, kernel_size,
                                           stride, padding, dilation, groups, bias)
-        self.rank = 3
-        self.addcoords = AddCoords(self.rank, with_r, use_cuda=use_cuda)
-        self.conv = nn.Conv3d(in_channels + self.rank + int(with_r), out_channels,
+        self.addcoords = AddCoords(with_r, use_cuda=use_cuda)
+        self.conv = nn.Conv3d(in_channels + 3 + int(with_r), out_channels,
                               kernel_size, stride, padding, dilation, groups, bias)
 
     def forward(self, input_tensor):
         """
-        input_tensor_shape: (N, C_in,H,W)
-        output_tensor_shape: N,C_out,H_out,W_out）
-        :return: CoordConv2d Result
+        input_tensor_shape: (N, C_in,H,W,D)
+        output_tensor_shape: N,C_out,H_out,W_out, D_out）
+        :return: CoordConv3d Result
         """
         out = self.addcoords(input_tensor)
         out = self.conv(out)
