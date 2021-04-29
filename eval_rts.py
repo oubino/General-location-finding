e44591e7279ed35bcbf22c7c12352684efbf8b92
rankinaaron98 <rankinaaron98@gmail.com>
2021-04-29 12:00:12 +0100

    merge

--- eval_rts.py
+++ eval_rts.py
@@ -3,42 +3,14 @@
 
 from useful_functs import yes_or_no
 import settings
-print('here')
-settings.init()
-<<<<<<< HEAD:eval_rts.py
-print('here 2')
 settings.root = r'/home/rankinaaron98/data/Facial_asymmetry_test_sets'
 settings.init_load()
-print('here 3')
 from data_loading import data_loaders
-print('here 4')
 from model import load_model   
-=======
 
-
->>>>>>> master:eval_reserved_test_set.py
-# cts and structures & csv path
-
-print('here 5')
-# evaluate model
-<<<<<<< HEAD:eval_rts.py
-'''downsample_q = input ("Downsample(y)/crop(n) ")
-if downsample_q == 'y':
-     downsample_user = True 
-elif downsample_q == 'n':  
-     downsample_user = False
-''' 
-
-=======
-downsample_q = input ("Downsample(y)/crop(n) ")
-if downsample_q == 'y':
-       downsample_user = True
-elif downsample_q == 'n':
-       downsample_user = False
->>>>>>> master:eval_reserved_test_set.py
 # initialise data loader
 data_loaders.init_reserved_test_set()
-print('here 6')
+
 # load in appropriate model
 load_transfered_model = yes_or_no.question('are you loading in a model which was saved as a transfered model')
 model = load_model.load_model(load_transfered_model)
