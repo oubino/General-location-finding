bf1c199b747a717a32a40bf417a328aa9c718a82
rankinaaron98 <rankinaaron98@gmail.com>
2021-05-05 21:35:29 +0100

    compare paed coords

---
+++ compare/pred_coords.py
@@ -0,0 +1,47 @@
+from useful_functs import functions as F
+import math
+root = r'/home/oli/data/results/oli/run_folder/eval_100_3'
+pred_coords_1 = F.load_obj_pickle(root, 'final_coords_no_struc')
+pred_coords_2 = F.load_obj_pickle(root, 'final_coords_no_struc_2')
+
+
+print(pred_coords_1)
+
+print('---------------------------------')
+
+print(pred_coords_2)
+
+
+dev_list = {}
+dev_list_x = {}
+dev_list_y = {}
+dev_list_z = {}
+
+mean_dev = {}
+mean_dev_x = {}
+mean_dev_y = {}
+mean_dev_z = {}
+mean_dev_std = {}
+mean_list = {}
+
+landmarks = [1,2,3,4,5,6,7,8,9,10]
+for i in pred_coords_1.keys(pred_coords_1):
+    for l in landmarks:
+        x_1 = pred_coords_1[i][l]['x']
+        x_2 = pred_coords_2[i][l]['x']
+        y_1 = pred_coords_2[i][l]['y']
+        y_2 = pred_coords_2[i][l]['y']
+        z_1 = pred_coords_2[i][l]['z']
+        z_2 = pred_coords_2[i][l]['z']
+        
+        x_dev = x_1 - x_2
+        y_dev = y_1 - y_2
+        z_dev = z_1 - z_2
+        dev = math.sqrt(abs(x_dev)**2 + abs(y_dev)**2 + abs(z_dev)**2)
+        print(dev)
+        dev_list['%1.0f' % i].append(dev)
+        dev_list_x['%1.0f' % i].append(x_dev)
+        dev_list_y['%1.0f' % i].append(y_dev)
+        dev_list_z['%1.0f' % i].append(z_dev)
+        
+        
