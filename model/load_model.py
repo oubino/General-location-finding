47bf66fe72f5c9e700882d1f86b12426aa166acb
oubino <oliver.umney@hotmail.co.uk>
2021-05-06 15:10:24 +0100

    trial initialising new final layer

--- model/load_model.py
+++ model/load_model.py
@@ -58,6 +58,10 @@ class load_model:
         list_sigma = [x for x in S.sigmas if x not in S.sigmas_load]
         for k in list_sigma:
             self.optimizer_load.add_param_group({'params': S.sigmas[k]}) 
+            
+        # change lr
+        self.optimizer_load.__setattr__('lr', 1e-4)
+        self.optimizer_load.__setattr__('weight_decay', 0.1)
          
         self.scheduler = lr_scheduler.StepLR(self.optimizer_load, step_size=20000, gamma=0.1)
         
