e03f4a0c04e482f082627366b4f2c8f67ac976eb
oubino <oliver.umney@hotmail.co.uk>
2021-05-08 18:50:26 +0100

    plot img

--- evaluate/evaluate_functions.py
+++ evaluate/evaluate_functions.py
@@ -340,11 +340,15 @@ def performance_metrics_line(model,sigmas,gamma, epochs_completed, fold):
                   val_max_list[patient[i]][l] = val_max[i] # update max val
                   coord_list[patient[i]][l]['x'], coord_list[patient[i]][l]['y'], coord_list[patient[i]][l]['z'] = pred_coords_max[i][0], pred_coords_max[i][1], pred_coords_max[i][2]                  
                   pat_index[patient[i]][l] = slide_index
-                  if val_max[i] > 0.7:
+                  if slide_index == 34 or slide_index == 35 or slide_index == 36:
                       print_2D_heatmap(image[i][0], l, pred[i][l-1], coord_list[patient[i]][l]['z'], eval_path, patient[i])
          
       S.slide_index += 1
   S.slide_index = 0
+  
+  print('val max')
+  print(val_max_list)
+  exit()
     
   # final locations dict
   final_loc = {}
@@ -542,7 +546,8 @@ def print_2D_heatmap(img, landmark, heatmap, pred_z, eval_path, patient):
     img = img[:, :, pred_z]
     
     heatmap = heatmap.detach().cpu()[:,:,pred_z]
-    heatmap = np.ma.masked_where(heatmap < 0.7, heatmap)
+    max_heatmap = np.max(heatmap) 
+    heatmap = np.ma.masked_where(heatmap < 0.6, heatmap)
     
     # ---- plot as point ------
     plt.imshow(img.cpu(),cmap = 'Greys_r', alpha = 0.9)
